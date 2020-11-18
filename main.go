c87a5cb030103aab7523bb8bc1df9b49dffc4a8d
Aman Brar <mabrara@amazon.com>
2020-11-18 13:51:59 -0500

    updating metrics over time

--- main.go
+++ main.go
@@ -2,7 +2,10 @@ package main
 
 import (
 	"fmt"
+	"math"
+	"math/rand"
 	"net/http"
+	"time"
 
 	"log"
 
@@ -16,64 +19,44 @@ var (
 	promRegistry = prometheus.NewRegistry() // local Registry so we don't get Go metrics, etc.
 )
 
+type metricBatch struct {
+	counter   prometheus.Counter
+	gauge     prometheus.Gauge
+	histogram prometheus.Histogram
+	summary   prometheus.Summary
+}
+
 func main() {
+	rand.Seed(time.Now().Unix())
+
 	healthCheck()
-	registerMetrics(metricCount)
+	metrics := registerMetrics(metricCount)
+	go updateMetrics(metrics)
 
 	http.Handle("/metrics", promhttp.HandlerFor(promRegistry, promhttp.HandlerOpts{}))
 
 	log.Fatal(http.ListenAndServe(":8080", nil))
 }
 
-func registerMetrics(metricCount int) {
-	if metricCount == 1 { // default for simple validation tests
-		counter := prometheus.NewCounter(
-			prometheus.CounterOpts{
-				Namespace: testingID,
-				Name:      "test_counter",
-				Help:      "This is my counter",
-			})
-		gauge := prometheus.NewGauge(
-			prometheus.GaugeOpts{
-				Namespace: testingID,
-				Name:      "test_gauge",
-				Help:      "This is my gauge",
-			})
-		histogram := prometheus.NewHistogram(
-			prometheus.HistogramOpts{
-				Namespace: testingID,
-				Name:      "test_histogram",
-				Help:      "This is my histogram",
-				Buckets:   []float64{0.005, 0.1, 1},
-			})
-		summary := prometheus.NewSummary(
-			prometheus.SummaryOpts{
-				Namespace: testingID,
-				Name:      "test_summary",
-				Help:      "This is my summary",
-				Objectives: map[float64]float64{
-					0.1:  0.5,
-					0.5:  0.5,
-					0.99: 0.5,
-				},
-			})
-
-		// Set arbitrary values
-		counter.Add(5)
-		gauge.Add(15)
-		for i := 0.005; i < 1; i += 0.005 {
-			histogram.Observe(i)
-			summary.Observe(i)
+func updateMetrics(metrics []*metricBatch) {
+	for {
+		for idx := range metrics {
+			metrics[idx].counter.Add(rand.Float64())
+			metrics[idx].gauge.Add(rand.Float64())
+			lowerBound := math.Mod(rand.Float64(), 1)
+			increment := math.Mod(rand.Float64(), 0.05)
+			for i := lowerBound; i < 1; i += increment {
+				metrics[idx].histogram.Observe(i)
+				metrics[idx].summary.Observe(i)
+			}
 		}
 
-		promRegistry.MustRegister(counter)
-		promRegistry.MustRegister(gauge)
-		promRegistry.MustRegister(histogram)
-		promRegistry.MustRegister(summary)
-		return
+		time.Sleep(time.Second)
 	}
+}
 
-	// For load tests we need an extra unique identifier, ie. index
+func registerMetrics(metricCount int) []*metricBatch {
+	metrics := make([]*metricBatch, metricCount)
 	for idx := 0; idx < metricCount; idx++ {
 		counter := prometheus.NewCounter(
 			prometheus.CounterOpts{
@@ -118,7 +101,11 @@ func registerMetrics(metricCount int) {
 		promRegistry.MustRegister(gauge)
 		promRegistry.MustRegister(histogram)
 		promRegistry.MustRegister(summary)
+
+		newMetrics := metricBatch{counter: counter, gauge: gauge, histogram: histogram, summary: summary}
+		metrics[idx] = &newMetrics
 	}
+	return metrics
 }
 
 func healthCheck() {
