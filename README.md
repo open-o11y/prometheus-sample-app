3836ab01dd6be3c140321377105b3e5fcd55f969
PaurushGarg <paurushg@amazon.com>
2021-12-15 22:50:50 -0800

    Adding code to address issue 10

--- README.md
+++ README.md
@@ -7,6 +7,8 @@ A health check endpoint also exists at `/`
 The following is a list of optional command line flags for configuration:
 * `listen_address`: (default = `0.0.0.0:8080`)this defines the address and port that the sample app is exposed to. This is primarily to conform with the test framework requirements.
 * `metric_count`: (default=1) the amount of each type of metric to generate. The same amount of metrics is always generated per metric type.
+* `label_count`: (default=1) the amount of labels per metric to generate.
+* `datapoint_count`: (default=1) the number of data-points per metric to generate. 
 
 Steps for running locally:
 ```bash
@@ -59,6 +61,14 @@ Usage of generate:
   
     	Type of metric (counter, gauge, histogram, summary) 
 
+  -label_count int
+
+    	Amount of labels to create per metric
+
+  -datapoint_count int
+
+    	Number of datapoints to create per metric
+
 Example: 
 ```bash
 $ docker build . -t prometheus-sample-app
@@ -70,3 +80,59 @@ $ docker build . -t prometheus-sample-app
 $ docker run -it -p 8080:8080 prometheus-sample-app /bin/main -listen_address=0.0.0.0:8080 generate -metric_type=all -is_random=true
 $ curl localhost:8080/metrics
 ```
+
+## Clustering:
+Deploy the example deployment configuration of 5 instances of Prometheus-Sample-App along with configured OTEL Collector.
+    
+### Pre-requisites:
+- Docker
+- Docker Image Prometheus-Sample-App
+- Minikube
+
+### Deployment on Kubernetes:
+- Run Docker
+- Start Minikube
+  ```bash
+    $ minikube start
+  ```
+- Start Minikube dashboard
+  ```bash
+    $ minikube dashboard
+  ```
+- Run following command to deploy: 
+    ```bash
+    $ kubectl apply -f otel-collector-k8s-deployment.yaml
+    $ kubectl create clusterrolebinding service-reader-pod --clusterrole=service-reader --serviceaccount=default:default
+    $ kubectl apply -f prometheus-sample-app-k8s-deployment.yaml
+    ```
+- Run following command to monitor logs from OTEL Collector Logging exporter :
+    ```bash
+    $ kubectl logs <otel-collector-pod-name>
+    ```
+### Deployment on EKS:
+- Create your cluster on EKS
+  ```bash
+  $ eksctl create cluster --name <cluster-name> --region <region> --with-oidc --ssh-access --ssh-public-key <public-key>
+  ```
+- Create repository on Amazon ECR to push docker image of Prometheus-Sample-App
+- Push the prometheus_sample_app docker image to this repository
+    ```bash
+    $ aws ecr get-login-password --region region | docker login --username AWS --password-stdin aws_account_id.dkr.ecr.region.amazonaws.com
+    $ docker build -t prometheus_sample_app .
+    $ docker tag prometheus_sample_app:latest aws_account_id.dkr.ecr.region.amazonaws.com/my-repository:tag
+    $ docker push aws_account_id.dkr.ecr.region.amazonaws.com/my-repository:tag
+    ```
+- Update imagePullPolicy of 'prometheus-sample-app-k8s-deployment.yaml' to IfNotPresent
+- Copy image URI from the AWS ECR repository and update in 'prometheus-sample-app-k8s-deployment.yaml'
+- Run following command to deploy:
+    ```bash
+    $ kubectl apply -f otel-collector-k8s-deployment.yaml
+    $ kubectl create clusterrolebinding service-reader-pod --clusterrole=service-reader --serviceaccount=default:default
+    $ kubectl apply -f prometheus-sample-app-k8s-deployment.yaml
+    ```
+- Run following command to monitor logs from OTEL Collector Logging exporter :
+    ```bash
+    $ kubectl logs <otel-collector-pod-name>
+    ```
+
+Currently, OTEL Collector is configured with Logging exporter. Since all 5 replica Prometheus-Sample-App pods will produce identical metric names and labels, so Prometheus Exporter should be configured with additional details to ensure distinct metrics identity. 
--- metrics/metrics_collector.go
+++ metrics/metrics_collector.go
@@ -1,6 +1,7 @@
 package metrics
 
 import (
+	"flag"
 	"fmt"
 	"log"
 	"math"
@@ -10,12 +11,20 @@ import (
 	"github.com/prometheus/client_golang/prometheus"
 )
 
+var (
+	normDomain = flag.Float64("normal.domain", 0.0002, "The domain for the normal distribution.")
+	normMean   = flag.Float64("normal.mean", 0.00001, "The mean for the normal distribution.")
+)
+
 type metricCollector struct {
-	counters   []prometheus.Counter
-	gauges     []prometheus.Gauge
-	histograms []prometheus.Histogram
-	summarys   []prometheus.Summary
-	interval   time.Duration
+	counters       []*prometheus.CounterVec
+	gauges         []*prometheus.GaugeVec
+	histograms     []*prometheus.HistogramVec
+	summarys       []*prometheus.SummaryVec
+	datapointCount int
+	labelValues    []string
+	labelKeys      []string
+	interval       time.Duration
 }
 
 var (
@@ -28,37 +37,43 @@ func newMetricCollector() metricCollector {
 
 func (mc *metricCollector) updateCounter() {
 	for _, c := range mc.counters {
-		c.Add(rand.Float64())
+		for i := 0; i < mc.datapointCount; i++ {
+			labels := datapointLabels(i, mc.labelKeys, mc.labelValues)
+			c.With(labels).Add(rand.Float64())
+		}
 	}
-
 }
 
 func (mc *metricCollector) updateGauge() {
 	for _, c := range mc.gauges {
-		c.Add(rand.Float64())
+		for i := 0; i < mc.datapointCount; i++ {
+			labels := datapointLabels(i, mc.labelKeys, mc.labelValues)
+			c.With(labels).Set(rand.Float64())
+		}
 	}
 }
 
 func (mc *metricCollector) updateHistogram() {
 	for idx := 0; idx < len(mc.histograms); idx++ {
-		lowerBound := math.Mod(rand.Float64(), 1)
-		increment := math.Mod(rand.Float64(), 0.05)
-		for i := lowerBound; i < 1; i += increment {
-			mc.histograms[idx].Observe(i)
-
+		for i := 0; i < mc.datapointCount; i++ {
+			lowerBound := math.Mod(rand.Float64(), 1)
+			increment := math.Mod(rand.Float64(), 0.05)
+			labels := datapointLabels(i, mc.labelKeys, mc.labelValues)
+			for j := lowerBound; j < 1; j += increment {
+				mc.histograms[idx].With(labels).Observe(j)
+			}
 		}
-	}
 
+	}
 }
+
 func (mc *metricCollector) updateSummary() {
 	for idx := 0; idx < len(mc.summarys); idx++ {
-		lowerBound := math.Mod(rand.Float64(), 1)
-		increment := math.Mod(rand.Float64(), 0.05)
-		for i := lowerBound; i < 1; i += increment {
-			mc.summarys[idx].Observe(i)
-
+		for i := 0; i < mc.datapointCount; i++ {
+			labels := datapointLabels(i, mc.labelKeys, mc.labelValues)
+			v := (rand.NormFloat64() * *normDomain) + *normMean
+			mc.summarys[idx].With(labels).Observe(v)
 		}
-
 	}
 }
 
@@ -93,13 +108,13 @@ func (mc *metricCollector) updateMetrics() {
 func (mc *metricCollector) registerCounter(count int) {
 	for idx := 0; idx < count; idx++ {
 		namespace := "test"
-		counter := prometheus.NewCounter(
+		counter := prometheus.NewCounterVec(
 			prometheus.CounterOpts{
 				Namespace: namespace,
 				Name:      fmt.Sprintf("counter%v", idx),
 				Help:      "This is my counter",
-			})
-
+			},
+			append([]string{"datapoint_id"}, mc.labelKeys...))
 		promRegistry.MustRegister(counter)
 		mc.counters = append(mc.counters, counter)
 	}
@@ -108,12 +123,13 @@ func (mc *metricCollector) registerCounter(count int) {
 func (mc *metricCollector) registerGauge(count int) {
 	for idx := 0; idx < count; idx++ {
 		namespace := "test"
-		gauge := prometheus.NewGauge(
+		gauge := prometheus.NewGaugeVec(
 			prometheus.GaugeOpts{
 				Namespace: namespace,
 				Name:      fmt.Sprintf("gauge%v", idx),
 				Help:      "This is my gauge",
-			})
+			},
+			append([]string{"datapoint_id"}, mc.labelKeys...))
 		promRegistry.MustRegister(gauge)
 		mc.gauges = append(mc.gauges, gauge)
 	}
@@ -122,13 +138,14 @@ func (mc *metricCollector) registerGauge(count int) {
 func (mc *metricCollector) registerHistogram(count int) {
 	for idx := 0; idx < count; idx++ {
 		namespace := "test"
-		histogram := prometheus.NewHistogram(
+		histogram := prometheus.NewHistogramVec(
 			prometheus.HistogramOpts{
 				Namespace: namespace,
 				Name:      fmt.Sprintf("histogram%v", idx),
 				Help:      "This is my histogram",
 				Buckets:   []float64{0.1, 0.5, 1},
-			})
+			},
+			append([]string{"datapoint_id"}, mc.labelKeys...))
 		promRegistry.MustRegister(histogram)
 		mc.histograms = append(mc.histograms, histogram)
 	}
@@ -137,18 +154,37 @@ func (mc *metricCollector) registerHistogram(count int) {
 func (mc *metricCollector) registerSummary(count int) {
 	for idx := 0; idx < count; idx++ {
 		namespace := "test"
-		summary := prometheus.NewSummary(
+		summary := prometheus.NewSummaryVec(
 			prometheus.SummaryOpts{
-				Namespace: namespace,
-				Name:      fmt.Sprintf("summary%v", idx),
-				Help:      "This is my summary",
-				Objectives: map[float64]float64{
-					0.1:  0.5,
-					0.5:  0.5,
-					0.99: 0.5,
-				},
-			})
+				Namespace:  namespace,
+				Name:       fmt.Sprintf("summary%v", idx),
+				Help:       "This is my summary",
+				Objectives: map[float64]float64{0.5: 0.05, 0.9: 0.01, 0.99: 0.001},
+			},
+			append([]string{"datapoint_id"}, mc.labelKeys...))
 		promRegistry.MustRegister(summary)
 		mc.summarys = append(mc.summarys, summary)
 	}
+}
+
+func generateLabels(labelCount int) ([]string, []string) {
+	labelKeys := make([]string, labelCount, labelCount)
+	for idx := 0; idx < labelCount; idx++ {
+		labelKeys[idx] = fmt.Sprintf("foo_%v", idx)
+	}
+	labelValues := make([]string, labelCount, labelCount)
+	for idx := 0; idx < labelCount; idx++ {
+		labelValues[idx] = fmt.Sprintf("bar_%v", idx)
+	}
+	return labelValues, labelKeys
+}
+
+func datapointLabels(datapointID int, labelKeys []string, labelValues []string) prometheus.Labels {
+	labelsDatapoint := prometheus.Labels{
+		"datapoint_id": fmt.Sprintf("%v", datapointID),
+	}
+	for idx, key := range labelKeys {
+		labelsDatapoint[key] = labelValues[idx]
+	}
+	return labelsDatapoint
 }
